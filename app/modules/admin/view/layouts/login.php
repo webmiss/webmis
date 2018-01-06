c42eb01313822dfee69d33279727cd3f20629a61
WebMIS <klingsoul@163.com>
2018-01-06 11:43:11 +0800

    perm

--- app/modules/admin/view/layouts/login.php
+++ app/modules/admin/view/layouts/login.php
@@ -13,8 +13,9 @@
 	<link rel="stylesheet" type="text/css" href="/themes/admin/main.css" />
 </head>
 
-<body class="body_login">
+<body>
 	<!--[if lt IE 9]><div class="isIE"><p><b>浏览器版本过低</b><br>请使用IE9以上或chrome、firefox、safari等最新版浏览器！</p></div><![endif]-->
+<div class="body_login">
 	<!--	Login -->
 	<div class="login_body">
 		<h1 id="webmisVersion" class="login_title">WebMIS</h1>
@@ -36,6 +37,7 @@
 			Copyright © WebMIS All rights are reserved.
 		</div>
 	</div>
+</div>
 <div id="BaseURL" style="display: none;"><?php echo self::getUrl();?></div>
 <script type="text/javascript" src="/webmis/plugin/jquery/jquery-3.min.js"></script>
 <script type="text/javascript" src="/webmis/jquery.webmis.js"></script>
--- public/db/mvc.sql
+++ public/db/mvc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 2017-07-25 13:29:46
--- 服务器版本： 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: 2018-01-06 11:33:36
+-- 服务器版本： 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,19 +28,17 @@ SET time_zone = "+00:00";
 -- 表的结构 `sys_admin`
 --
 
-DROP TABLE IF EXISTS `sys_admin`;
-
 CREATE TABLE `sys_admin` (
   `id` tinyint(3) NOT NULL COMMENT 'ID',
   `uname` varchar(16) NOT NULL COMMENT '用户名',
   `password` varchar(32) NOT NULL COMMENT '密码',
-  `email` varchar(32) DEFAULT NULL COMMENT '邮箱',
-  `tel` varchar(11) DEFAULT NULL COMMENT '电话',
-  `name` varchar(12) DEFAULT NULL COMMENT '姓名',
-  `department` varchar(12) DEFAULT NULL COMMENT '部门',
-  `position` varchar(12) DEFAULT NULL COMMENT '职称',
+  `email` varchar(32) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `tel` varchar(11) NOT NULL DEFAULT '' COMMENT '电话',
+  `name` varchar(12) NOT NULL DEFAULT '' COMMENT '姓名',
+  `department` varchar(12) NOT NULL DEFAULT '' COMMENT '部门',
+  `position` varchar(12) NOT NULL DEFAULT '' COMMENT '职称',
   `rtime` datetime DEFAULT NULL COMMENT '注册时间',
-  `state` varchar(1) DEFAULT '1' COMMENT '状态(1正常,2禁用)',
+  `state` enum('1','2') NOT NULL DEFAULT '1' COMMENT '状态(1正常,2禁用)',
   `perm` text COMMENT '权限'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -57,18 +55,16 @@ INSERT INTO `sys_admin` (`id`, `uname`, `password`, `email`, `tel`, `name`, `dep
 -- 表的结构 `sys_menus`
 --
 
-DROP TABLE IF EXISTS `sys_menus`;
-
 CREATE TABLE `sys_menus` (
   `id` tinyint(3) NOT NULL COMMENT 'ID',
-  `fid` varchar(3) NOT NULL COMMENT '父ID',
+  `fid` tinyint(3) NOT NULL COMMENT '父ID',
   `title` varchar(12) NOT NULL COMMENT '标题',
-  `url` varchar(32) DEFAULT NULL COMMENT '地址',
-  `perm` varchar(6) DEFAULT NULL COMMENT '预设权限',
-  `ico` varchar(16) DEFAULT NULL COMMENT '图标',
+  `url` varchar(32) NOT NULL DEFAULT '' COMMENT '地址',
+  `perm` varchar(6) NOT NULL DEFAULT '' COMMENT '预设权限',
+  `ico` varchar(16) NOT NULL DEFAULT '' COMMENT '图标',
   `ctime` datetime DEFAULT NULL COMMENT '创建时间',
   `sort` tinyint(3) NOT NULL DEFAULT '0' COMMENT '排序',
-  `remark` varchar(32) DEFAULT NULL COMMENT '备注'
+  `remark` varchar(32) NOT NULL DEFAULT '' COMMENT '备注'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -76,16 +72,16 @@ CREATE TABLE `sys_menus` (
 --
 
 INSERT INTO `sys_menus` (`id`, `fid`, `title`, `url`, `perm`, `ico`, `ctime`, `sort`, `remark`) VALUES
-(1, '0', '首页', 'Welcome', '0', 'ico-home', '2017-05-12 00:00:00', 0, NULL),
-(2, '0', '系统', 'System', '0', 'ico-system', '2017-05-12 00:00:00', 0, NULL),
-(3, '1', '桌面', '#', '0', 'ico-disktop', '2017-05-12 00:00:00', 0, ''),
-(4, '2', '系统管理', '#', '0', 'ico-system1', '2017-05-12 00:00:00', 0, NULL),
-(5, '3', '用户首页', 'Desktop', '0', 'ico-user', '2017-05-12 00:00:00', 0, ''),
-(6, '4', '菜单管理', 'SysMenus', '31', 'ico-menu', '2017-05-12 00:00:00', 0, ''),
-(7, '4', '菜单动作', 'SysMenusAction', '31', 'ico-menuA', '2017-05-12 00:00:00', 0, ''),
-(8, '4', '系统用户', 'SysAdmins', '63', 'ico-pwd', '2017-05-12 00:00:00', 0, ''),
-(9, '4', '系统配置', 'SysConfig', '0', 'ico-system2', '2017-05-12 00:00:00', 0, ''),
-(10, '0', '网站', 'Web', '0', 'ico-web', NULL, 0, '');
+(1, 0, '首页', 'Welcome', '0', 'ico-home', '2017-05-12 00:00:00', 0, ''),
+(2, 0, '系统', 'System', '0', 'ico-system', '2017-05-12 00:00:00', 0, ''),
+(3, 1, '桌面', '#', '0', 'ico-disktop', '2017-05-12 00:00:00', 0, ''),
+(4, 2, '系统管理', '#', '0', 'ico-system1', '2017-05-12 00:00:00', 0, ''),
+(5, 3, '用户首页', 'Desktop', '0', 'ico-user', '2017-05-12 00:00:00', 0, ''),
+(6, 4, '菜单管理', 'SysMenus', '31', 'ico-menu', '2017-05-12 00:00:00', 0, ''),
+(7, 4, '菜单动作', 'SysMenusAction', '31', 'ico-menuA', '2017-05-12 00:00:00', 0, ''),
+(8, 4, '系统用户', 'SysAdmins', '63', 'ico-pwd', '2017-05-12 00:00:00', 0, ''),
+(9, 4, '系统配置', 'SysConfig', '0', 'ico-system2', '2017-05-12 00:00:00', 0, ''),
+(10, 0, '网站', 'Web', '0', 'ico-web', NULL, 0, '');
 
 -- --------------------------------------------------------
 
@@ -93,12 +89,10 @@ INSERT INTO `sys_menus` (`id`, `fid`, `title`, `url`, `perm`, `ico`, `ctime`, `s
 -- 表的结构 `sys_menus_action`
 --
 
-DROP TABLE IF EXISTS `sys_menus_action`;
-
 CREATE TABLE `sys_menus_action` (
   `id` int(2) NOT NULL COMMENT 'ID',
   `name` varchar(32) NOT NULL COMMENT 'Name',
-  `perm` varchar(6) NOT NULL COMMENT 'Authority',
+  `perm` enum('1','2','4','8','16','32','64','128','256','512','1024','2048') NOT NULL COMMENT 'Authority',
   `ico` varchar(24) DEFAULT NULL COMMENT 'ICON'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -148,16 +142,19 @@ ALTER TABLE `sys_menus_action`
 --
 ALTER TABLE `sys_admin`
   MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=2;
+
 --
 -- 使用表AUTO_INCREMENT `sys_menus`
 --
 ALTER TABLE `sys_menus`
   MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=11;
+
 --
 -- 使用表AUTO_INCREMENT `sys_menus_action`
 --
 ALTER TABLE `sys_menus_action`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=10;COMMIT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=10;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
