bf4ad2e9dd997e353412fe204193dfc84e0494cd
webmiss <klingsoul@163.com>
2018-04-09 10:54:18 +0800

    bug

--- app/modules/admin/view/system/admin/perm.php
+++ app/modules/admin/view/system/admin/perm.php
@@ -37,7 +37,7 @@ foreach($m2->menus as $m3){
 <?php
 foreach($Perm as $val){
 	if(intval($m3->perm)&intval($val->perm)){
-		$checked = isset($permArr[$m3->id])?' checked':'';
+		$checked = intval($permArr[$m3->id])&intval($val->perm)?' checked':'';
 ?>
 			<span class="text"><input type="checkbox" value="<?php echo $val->perm;?>"<?php echo $checked;?> /></span>
 			<span><?php echo $val->name;?></span>
